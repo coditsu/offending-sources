6e0c8d4f072d4ac080982d493d8d2aef48069de2
Maciej Mensfeld <maciej@mensfeld.pl>
2017-08-31 21:55:36 +0200

    gem bump

--- karafka.rb
+++ karafka.rb
@@ -10,7 +10,7 @@ class App < Karafka::App
   setup do |config|
     config.client_id = Settings.client_id
     config.redis = Settings.redis.symbolize_keys
-    config.processing_backend = Karafka.env.development? ? :inline : :sidekiq
+    config.backend = Karafka.env.development? ? :inline : :sidekiq
     config.kafka.seed_brokers = Settings.kafka.seed_brokers
     config.kafka.offset_commit_threshold = Settings.kafka.offset_commit_threshold
     config.monitor = KarafkaMonitor.instance
