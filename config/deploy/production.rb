3fb9d4aad18ef3185b6a1c3082aef6ac0bb8642e
Maciej Mensfeld <maciej@mensfeld.pl>
2018-09-13 15:46:40 +0200

    gcloud

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,20 +1,17 @@
 # frozen_string_literal: true
 
-server 'coditsu-production-frontend',
-       roles: %w[web],
+server 'orchestration-vm1.prod.coditsu.it',
+       roles: %w[web app db karafka],
        user: 'deploy'
 
-server 'coditsu-production-main',
-       roles: %w[app db karafka],
-       user: 'deploy'
-
-set :ssh_options,
-    port: 2222,
-    forward_agent: true
+set :ssh_options, forward_agent: true
 
 set :stage, :production
 set :environment, 'production'
 set :rails_env, fetch(:environment)
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+set :settings_path, "config/deploy/#{fetch(:stage)}"
 
 set :tmp_dir, '/home/deploy/.tmp'
 set :deploy_to, "/home/deploy/coditsu/#{fetch(:application)}"
@@ -26,9 +23,7 @@ set :puma_conf,  "#{shared_path}/config/puma.rb"
 set :puma_pid,   "#{shared_path}/tmp/pids/puma"
 set :puma_state, "#{shared_path}/tmp/sockets/pumastate"
 
-set :rvm_custom_path, '/usr/local/rvm'
-set :rvm_ruby_version, "2.5.1@#{fetch(:application)}"
+set :branch, ENV['REVISION'] || ENV['BRANCH_NAME'] || 'master'
+set :domain, 'orchestration-vm1.prod.coditsu.it'
 
-set :branch, ENV.fetch('REVISION') { ENV.fetch('BRANCH_NAME', 'master') }
-set :domain, 'offending-sources.coditsu.io'
-set :whenever_roles, %w[web]
+set :whenever_roles, %i[app db web]
