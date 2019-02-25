2e9fe53739297cc7f2302315b5483f7b2d996999
Maciej Mensfeld <maciej@mensfeld.pl>
2019-02-25 14:52:10 +0100

    better querying

--- app/concepts/ruby/gems_licenser/select_licenses.rb
+++ app/concepts/ruby/gems_licenser/select_licenses.rb
@@ -9,7 +9,7 @@ module Ruby
     #   result = Ruby::GemsLicenser::SelectLicenses.call(gems)
     #   result['model'] #=> { "nokogiri"=>"---|||- MIT|||" }
     class SelectLicenses < ApplicationOperation
-      step :select_rubygems
+      step :extract_gems_with_versions
       step :prepare_search_scope
       step :select_licences_for_requested_versions
       step :select_licenses_for_the_most_recent
@@ -22,18 +22,13 @@ module Ruby
       # Selects gems and their ids
       # @param ctx [Trailblazer::Skill]
       # @param params [Hash<String, String>] hash where we get the name of the gem and it's version
-      def select_rubygems(ctx, params:, **)
-        ctx['gems'] = Ruby::RubyGem
-                      .where(name: params.keys.first(Settings.max_gems_per_request))
-                      .select(:name, :id)
-        ctx['gems_ids'] = ctx['gems'].map(&:id)
-        ctx['versions'] = params.values
+      def extract_gems_with_versions(ctx, params:, **)
+        ctx['gems_with_versions'] = params.first(Settings.max_gems_per_request)
       end
 
       # Prepares the basic search scope that pics the licenses for requested gems
       # @param ctx [Trailblazer::Skill]
-      # @param gems_ids [Array<Integer>] array with ids of gems in which we're interested
-      def prepare_search_scope(ctx, gems_ids:, **)
+      def prepare_search_scope(ctx, **)
         ctx['search_scope'] = \
           Ruby::RubyGem
           .select(
@@ -46,38 +41,44 @@ module Ruby
           )
           .joins('INNER JOIN versions ON rubygems.id = versions.rubygem_id')
           .where('yanked_at IS NULL')
-          .where(id: gems_ids)
-          .order('rubygems.id, versions.updated_at::date DESC')
       end
 
       # Searches for licenses for the exact versions of gems that were requested
       # @param ctx [Trailblazer::Skill]
-      # @param versions [Array<String>] requested gem versions
-      def select_licences_for_requested_versions(ctx, versions:, **)
+      # @param gems_with_versions [Array<Array<String, String>>] requested gems with versions
+      def select_licences_for_requested_versions(ctx, gems_with_versions:,**)
+        query = gems_with_versions
+                .map { '(rubygems.name = ? AND versions.number = ?)' }
+                .join(' OR ')
+
         ctx['requested'] = \
           ctx['search_scope']
-          .where('versions.number IN (?)', versions)
+          .where(query, *gems_with_versions.flatten)
           .each_with_object({}) { |gem, accu| accu[gem.name] = gem }
       end
 
       # Fallback method in case we were unable to find a license for requested gem version,
       #   then we pick the most recent license there is for those gems
       # @param ctx [Trailblazer::Skill]
-      def select_licenses_for_the_most_recent(ctx, **)
+      # @param gems_with_versions [Array<Array<String, String>>] requested gems with versions
+      def select_licenses_for_the_most_recent(ctx, gems_with_versions:, **)
         ctx['most_recent'] = \
           ctx['search_scope']
+          .order('rubygems.id, versions.updated_at::date DESC')
+          .where('name IN (?)', gems_with_versions.map(&:first))
           .each_with_object({}) { |gem, accu| accu[gem.name] = gem }
       end
 
       # Combines fetched data into a hash where the key is the gem name and the value is
       #   the license
       # @param ctx [Trailblazer::Skill]
-      # @param gems [Array<Ruby::RubyGem>] gems we've requested
+      # @param gems_with_versions [Array<Array<String, String>>] requested gems with versions
       # @param requested [Array<Ruby::RubyGem>] licenses for requested gems versions
       # @param most_recent [Array<Ruby::RubyGem>] fallback licenses from the most recent versions
-      def combine_results(ctx, gems:, requested:, most_recent:, **)
-        ctx['model'] = gems.each_with_object({}) do |gem, accu|
-          accu[gem.name] = (requested[gem.name] || most_recent[gem.name])&.licenses
+      def combine_results(ctx, gems_with_versions:, requested:, most_recent:, **)
+        ctx['model'] = gems_with_versions.each_with_object({}) do |gem_data, accu|
+          name = gem_data.first
+          accu[name] = (requested[name] || most_recent[name])&.licenses
         end
       end
     end
