62202647e05ec0c2034a22052e58a0485b569e56
Nikita Pupko <lazynick7@gmail.com>
2019-03-07 22:21:59 +0300

    Add spec for most resent versions selector

--- spec/factories/ruby/versions.rb
+++ spec/factories/ruby/versions.rb
@@ -2,5 +2,10 @@
 
 FactoryBot.define do
   factory :ruby_version, class: 'Ruby::Version' do
+    trait :with_different_numbers do
+      sequence :number do |n|
+        "#{n}.0.0"
+      end
+    end
   end
 end
