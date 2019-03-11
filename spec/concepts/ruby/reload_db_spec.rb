fee7f7c6603da49611128342c61e52fb0c382d76
Maciej Mensfeld <maciej@mensfeld.pl>
2019-03-11 17:58:04 +0100

    nicer mock

--- spec/concepts/ruby/reload_db_spec.rb
+++ spec/concepts/ruby/reload_db_spec.rb
@@ -1,38 +1,39 @@
 # frozen_string_literal: true
 
 RSpec.describe Ruby::ReloadDb do
+  subject(:reloading) { described_class.call }
+
+  let(:instance) { described_class.new }
+
+  before { allow(described_class).to receive(:new).and_return(instance) }
+
+  after { reloading }
+
   it 'has DB_USERNAME env variable' do
-    expect_any_instance_of(Kernel).to receive(:system).with(/DB_USERNAME/)
-    described_class.call
+    expect(instance).to receive(:system).with(/DB_USERNAME/)
   end
 
   it 'has DB_PASSWORD env variable' do
-    expect_any_instance_of(Kernel).to receive(:system).with(/DB_PASSWORD/)
-    described_class.call
+    expect(instance).to receive(:system).with(/DB_PASSWORD/)
   end
 
   it 'has DB_HOST env variable' do
-    expect_any_instance_of(Kernel).to receive(:system).with(/DB_HOST/)
-    described_class.call
+    expect(instance).to receive(:system).with(/DB_HOST/)
   end
 
   it 'has DB_USERNAME env variable' do
-    expect_any_instance_of(Kernel).to receive(:system).with(/DB_USERNAME/)
-    described_class.call
+    expect(instance).to receive(:system).with(/DB_USERNAME/)
   end
 
   it 'has DB_PORT env variable' do
-    expect_any_instance_of(Kernel).to receive(:system).with(/DB_PORT/)
-    described_class.call
+    expect(instance).to receive(:system).with(/DB_PORT/)
   end
 
   it 'has DB_NAME env variable' do
-    expect_any_instance_of(Kernel).to receive(:system).with(/DB_NAME/)
-    described_class.call
+    expect(instance).to receive(:system).with(/DB_NAME/)
   end
 
   it 'has RAILS_ROOT env variable' do
-    expect_any_instance_of(Kernel).to receive(:system).with(/RAILS_ROOT/)
-    described_class.call
+    expect(instance).to receive(:system).with(/RAILS_ROOT/)
   end
 end
