5ae3c6498091754142381aa37bf6d47fa799c11b
Nikita Pupko <lazynick7@gmail.com>
2019-03-15 19:49:15 +0300

    Add specs for controllers

--- spec/controllers/ruby/outdated_gems_controller_spec.rb
+++ spec/controllers/ruby/outdated_gems_controller_spec.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
-RSpec.describe Ruby::OutdatedGemsController do
-  pending
+RSpec.describe Ruby::OutdatedGemsController, type: :controller do
+  let(:result) do
+    {
+      'karafka': [
+        '1.2.11',
+        '1.2.0.beta4'
+      ]
+    }
+  end
+
+  specify do
+    get :index, params: { data: ['karafka'] }
+
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'calls AbandonedGems::SelectLastReleasedDates with correct params' do
+    expect(Ruby::OutdatedGems::SelectMostRecentVersions)
+      .to receive(:call).with(['karafka']).and_return('model' => result)
+
+    get :index, params: { data: ['karafka'] }
+  end
 end
