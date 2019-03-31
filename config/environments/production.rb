a597ff4b5386d8c348db79e1f684a726227edb60
Maciej Mensfeld <maciej@mensfeld.pl>
2019-03-31 18:40:14 +0200

    remarks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,17 +22,6 @@ Rails.application.configure do
 
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-
-  # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :warn
